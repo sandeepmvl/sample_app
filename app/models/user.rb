3b5a721273372c7a2599b5d33121771cb4d4772b
sandeep <sandeep.mvl@gmail.com>
2013-01-14 00:11:02 +0530

    added some styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,13 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :uname
   # attr_accessible :title, :body
+  
+  attr_accessible :avatar
+    has_attached_file :avatar, :styles => {
+        :thumb => "80x80#",
+        :small  => "150x150>",
+        :medium => "200x200" ,:san =>"30x30#"}
+  validates_attachment :avatar,
+    :content_type => { :content_type=>['image/jpeg', 'image/png', 'image/gif'] },
+    :size => { :in => 0..4000.kilobytes }
 end
